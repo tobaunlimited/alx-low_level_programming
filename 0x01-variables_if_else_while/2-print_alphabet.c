119d4bf9e65fea66f8e5f7a7589f3ba877de51b3
Falade Toba <writing.isatool@gmail.com>
2023-03-20 05:02:55 -0700

    add  2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    char letter = 'a';
-    int i;
+	char letter = 'a';
+	int i;
 
-    for (i = 0; i < 26; i++)
-    {
-        putchar(letter);
-        letter++;
-    }
+	for (i = 0; i < 26; i++)
+	{
+		putchar(letter);
+		letter++;
+	}
 
-    putchar('\n');
-    return (0);
+	putchar('\n');
+
+	return (0);
 }
