cbcfbc29e27a9b59b2a7f351481ec975605bff3b
Falade Toba <writing.isatool@gmail.com>
2023-03-20 04:43:29 -0700

    add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter = 'a';
+    int i;
+
+    for (i = 0; i < 26; i++) {
+        putchar(letter);
+        letter++;
+    }
+
+    putchar('\n');
+    return 0;
+}
