98db5417f0d8ee244f711c4ec1f5108854fde935
Falade Toba <writing.isatool@gmail.com>
2023-03-20 05:56:17 -0700

    add 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,10 +7,12 @@
  */
 int main(void)
 {
-        int num;
+        int i;
 
-        for (num = 0; num < 10; num++)
-                putchar(num + '0');
+        for (i = 0; i < 10; i++)
+        {
+                putchar(i + '0');
+        }
         putchar('\n');
 
         return (0);
