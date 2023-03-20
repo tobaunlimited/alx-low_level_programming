1a304e297bf9ea866c89d744f9eb09a5c9da4596
Falade Toba <writing.isatool@gmail.com>
2023-03-20 04:53:15 -0700

    add 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,17 +7,20 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
     char letter = 'a';
     int i;
 
     for (i = 0; i < 26; i++)
-   : {
+
+   {
         putchar(letter);
+
         letter++;
     }
 
     putchar('\n');
-    return 0;
+    return (0);
 }
