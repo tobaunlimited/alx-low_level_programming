974f600fc4804ae0a13112107c36138016a8489f
Falade Toba <writing.isatool@gmail.com>
2023-03-20 00:44:05 -0700

    add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
-int main(void) {
-  char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, msg, 50);
-  return 1;
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19/n", 59);
+	return (1);
 }
 
