66dbaae0685b5ec1b374906bcdc64c92ba75f26f
Falade Toba <writing.isatool@gmail.com>
2023-03-20 20:19:07 -0700

    add 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,18 +2,18 @@
 
 /**
  * main - Prints all single digit numbers of base 10 starting from 0,
- *        followed by a new line.
+ * followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + '0');
-    }
-    putchar('\n');
-    return (0);
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
 }
