e358947c052be0f57033dc56e77c52e8f148e30d
Falade Toba <writing.isatool@gmail.com>
2023-03-22 21:38:58 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: The number to check
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n >
+
